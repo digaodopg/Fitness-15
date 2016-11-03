6632d06c3c4e54ae5cc6d1087b6f19f3f6a9b0f3
thzidane <hao.tang.ecust@gmail.com>
2016-11-02 20:14:55 -0500

    Update on dashboard

---
+++ fitness fifteen/DashboardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardTableViewCell.swift
+//  fitness fifteen
+//
+//  Created by Hao Tang on 11/2/16.
+//  Copyright © 2016 Hao Tang. All rights reserved.
+//
+
+import UIKit
+
+class DashboardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
